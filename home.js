77af0ab863464f769aef862c64a653259c384b74
Ayush Jalan <90421936+The15thSin@users.noreply.github.com>
2022-02-16 02:08:10 +0530

    updated

--- home.js
+++ home.js
@@ -1,65 +1,10 @@
 /* Set the width of the side navigation */
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "280px";
+  document.getElementById("mySidenav").style.width = "200px";
 }
 
 /* Set the width of the side navigation */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0px";
-}
-
-var responsiveSlider = function(){
-var slider = document.getElementById("slider");
-var sliderWidth = slider.offsetWidth;
-var slideList = document.getElementById("slideWrap");
-var count = 1;
-var items = slideList.querySelectorAll("li").length;
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
-
-window.addEventListener('resize', function() {
-  sliderWidth = slider.offsetWidth;
-});
-
-var prevSlide = function() {
-  if(count > 1) {
-    count = count - 2;
-    slideList.style.left = "-" + count * sliderWidth + "px";
-    count++;
-  }
-  else if(count = 1) {
-    count = items - 1;
-    slideList.style.left = "-" + count * sliderWidth + "px";
-    count++;
-  }
-};
-
-var nextSlide = function() {
-  if(count < items) {
-    slideList.style.left = "-" + count * sliderWidth + "px";
-    count++;
-  }
-  else if(count = items) {
-    slideList.style.left = "0px";
-    count = 1;
-  }
-};
-
-next.addEventListener("click", function() {
-  nextSlide();
-});
-
-prev.addEventListener("click", function() {
-  prevSlide();
-});
-
-setInterval(function() {
-  nextSlide()
-}, 5000);
-
-};
-
-window.onload = function() {
-responsiveSlider();
 }
